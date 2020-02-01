2d29126a0dbcc588700a11ce5f440e480778e2ed
Jesse Paquette <jesse@tag.bio>
2020-01-31 18:24:40 -0800

    Added a suggestion for installing, and a .gitignore

---
+++ install.sh
@@ -0,0 +1,10 @@
+# steps
+
+# 1. Install R (apt?)
+# Kenn add install line here
+
+# 2. Install r-utils library and packages
+R CMD INSTALL tagbio_0.1.0.tar.gz
+
+# 3. Install packages for code that will run here
+R CMD $FC_REPO_PATH/R/install_packages.R
