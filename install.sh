be3e60f6d1e84c601a776d162d45e6d13dab2b15
Kenn Brodhagen <kenn@tag.bio>
2020-02-04 11:51:49 -0500

    install.sh adds dependencies

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
 set -e
 
+# package dependencies
+apt-get update
+apt-get install -y \
+  libcurl4-openssl-dev \
+  libssl-dev \
+  python3
+
 install.r argparse gridExtra httr qpdf rjson
 install.r $TAGBIO_R_UTILS/tagbio_0.2.0.tar.gz
 
