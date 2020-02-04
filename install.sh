59ddd876c243ea0477a69540c25f1e57bf1034aa
Kenn Brodhagen <kenn@tag.bio>
2020-02-04 13:44:41 -0500

    install.sh adds tidyverse

--- install.sh
+++ install.sh
@@ -2,12 +2,17 @@
 set -e
 
 # package dependencies
+echo "Installing tagbio R system dependencies"
 apt-get update
 apt-get install -y \
   libcurl4-openssl-dev \
   libssl-dev \
+  libxml2-dev \
   python3
 
-install.r argparse gridExtra httr qpdf rjson
+echo "Installing tagbio R package dependencies"
+install.r argparse gridExtra httr qpdf rjson tidyverse
+
+echo "Installing tagbio R package itself"
 install.r $TAGBIO_R_UTILS/tagbio_0.2.0.tar.gz
 
