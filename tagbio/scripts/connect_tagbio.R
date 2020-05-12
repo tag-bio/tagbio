7d9c3f15daa6947b5b4d6b307061fae8cbaade2c
J Ireland <jireland@alternateallele.com>
2020-05-12 07:26:36 -0700

    Updated with message file and reworked file types

--- tagbio/scripts/connect_tagbio.R
+++ tagbio/scripts/connect_tagbio.R
@@ -16,35 +16,16 @@ parser$add_argument("-d", "--fc_data", required = TRUE,
     help="JSON file specifying FC and protocol.")
 parser$add_argument("-f", "--user_function", required = TRUE,
     help="User function passed in a R file.")
-parser$add_argument("-o", "--output_file", required = FALSE, 
+parser$add_argument("-m", "--message_file", required = FALSE,
+    help="Save messages to a file at this path.")
+parser$add_argument("-o", "--output_file", required = TRUE, 
     help="Save result file to this path.")
-parser$add_argument("-t", "--output_type", required = FALSE, 
-    help="File type of result.", choices = c("jpg", "html", "pdf", "png", "svg"))
+parser$add_argument("-t", "--output_type", required = TRUE, 
+    help="File type of result.", choices = c("html", "json", "pdf", "png"))
 
 args <- parser$parse_args()
 
 print("Starting R process")
-
-## method for adding file paths to tag result
-add_result_file <- function(tag_res, file_path, file_type) {
-    if (file_type == 'pdf') {
-        tag_res@pdf <- file_path
-    }
-    if (file_type == 'png') {
-        tag_res@png <- file_path
-    }
-    if (file_type == 'jpeg') {
-        tag_res@jpeg <- file_path
-    }
-    if (file_type == 'svg') {
-        tag_res@svg <- file_path
-    }
-    if (file_type == 'html') {
-        tag_res@html <- file_path
-    }
-    return(tag_res)
-}
-
 
 ## Read in user function
 user_function <- dget(args$user_function)
@@ -71,7 +52,10 @@ tag_data <- getTagData(fc, prot_inst, script)
 tag_data@parameters <- fc_data$passthrough_arguments
 
 ## Set up a tag result object with file paths
-tag_result <- TagbioResult()
+tag_result <- TagbioResult(output_path = args$output_file, result_type = args$output_type)
+if (!is.null(args$message_file)) {
+    tag_result@message_path <- args$message_file
+}
 
 # apply pass through arguments if exist
 params <- tag_data@parameters
@@ -79,17 +63,6 @@ output_set <- FALSE
 if (!is.null(params) & !is.null(params$output_file) & !is.null(params$output_type)) {
     tag_result <- add_result_file(tag_result, params$output_file[1], params$output_type[1])
     output_set <- TRUE
-}
-
-# apply commandline arguments if set
-if (!is.null(args$output_file) & !is.null(args$output_type)) {
-    tag_result <- add_result_file(tag_result, args$output_file, args$output_type)
-    output_set <- TRUE
-}
-
-if (!output_set) {
-    print("ERROR:  No output file or output type specified.")
-    quit(save = "no", status = 1)
 }
 
 ## Run the user function.  Should return a tag result back
