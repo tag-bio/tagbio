5245e338e50bb7dd19188e153d013655a200d41d
Kenn Brodhagen <kenn@tag.bio>
2020-07-29 17:56:19 -0400

    install.sh takes param for tagbio lib version.  install-r.sh added, takes param for R version.  install-bioc-checked.R added, installs bioconductor lib and fails if not installed.  Dockerfile-test added to test all of it.

---
+++ install-r.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+set -e
+
+export R_BASE_VERSION=${1:-3.6.3}
+echo "Installing R $R_BASE_VERSION"
+
+apt-get update -y
+apt-get install -y \
+  ed \
+  ca-certificates \
+  fonts-texgyre \
+  gnupg \
+  gnupg2 \
+  less \
+  locales \
+  vim-tiny \
+  wget
+
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen en_US.utf8
+/usr/sbin/update-locale LANG=en_US.UTF-8
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
+echo "deb http://cloud.r-project.org/bin/linux/debian buster-cran35/" >> /etc/apt/sources.list
+echo "deb http://cloud.r-project.org/bin/linux/debian buster-cran40/" >> /etc/apt/sources.list
+# echo "deb http://http.debian.net/debian sid main" > /etc/apt/sources.list.d/debian-unstable.list
+# echo 'APT::Default-Release "testing";' > /etc/apt/apt.conf.d/default
+
+apt-get update
+apt-get install -y \
+  littler \
+  r-cran-littler \
+  r-base=${R_BASE_VERSION}-* \
+  r-base-dev=${R_BASE_VERSION}-* \
+  r-recommended=${R_BASE_VERSION}-*
+
+ln -s /usr/lib/R/site-library/littler/examples/install.r /usr/local/bin/install.r
+ln -s /usr/lib/R/site-library/littler/examples/install2.r /usr/local/bin/install2.r
+ln -s /usr/lib/R/site-library/littler/examples/installGithub.r /usr/local/bin/installGithub.r
+ln -s /usr/lib/R/site-library/littler/examples/testInstalled.r /usr/local/bin/testInstalled.r
+install.r docopt
--- install.sh
+++ install.sh
@@ -2,18 +2,31 @@
 set -e
 set -x
 
+TAGBIO_R_VERSION=${1:-0.8.1}
+echo "Installing tagbio R version $TAGBIO_R_VERSION"
+
 # package dependencies
 echo "Installing tagbio R system dependencies"
 apt-get update
 apt-get install -y \
+  libcairo2-dev \
   libcurl4-openssl-dev \
+  libfontconfig1-dev \
   libssl-dev \
   libxml2-dev \
   python3
 
 echo "Installing tagbio R package dependencies"
-install.r argparse gridExtra httr qpdf rjson tidyverse svglite
+# do them one at a time for early failure
+install2.r -e argparse
+install2.r -e BiocManager
+install2.r -e gridExtra
+install2.r -e httr
+install2.r -e qpdf
+install2.r -e rjson
+install2.r -e tidyverse
+install2.r -e svglite
 
 echo "Installing tagbio R package itself"
-install.r $TAGBIO_R_UTILS/tagbio_0.8.1.tgz
+install2.r -e $TAGBIO_R_UTILS/tagbio_$TAGBIO_R_VERSION.tgz
 
