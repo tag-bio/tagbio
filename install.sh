7b37066656868c154a2dd53f887fac51f6c61079
Kenn Brodhagen <kenn@tag.bio>
2021-02-15 21:33:45 +0000

    install.sh edited: comment conda update

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ apt-get install -y \
 
 conda config --add channels bioconda
 conda config --add channels conda-forge
-conda update --all -y
+# conda update --all -y
 conda install \
   bioconductor-gsva \
   docopt \
