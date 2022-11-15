ed94aa0d84b83a4945c5e1066d7ec0e95c6ee111
Derek <derek@tag.bio>
2022-11-15 09:56:45 -0500

    switch to conda over mamba

--- install-r.sh
+++ install-r.sh
@@ -4,7 +4,7 @@ set -e
 export R_BASE_VERSION=${1:-4.1.3}
 export R_MAJOR=$(echo $R_BASE_VERSION|cut -b 1)
 
-mamba install -y -c conda-forge \
+conda install -y -c conda-forge \
   r-base=$R_BASE_VERSION \
   r-essentials=4.1 \
   r-littler=0.3.17 
