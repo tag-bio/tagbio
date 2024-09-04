e4c72153c91045320b100abb120905f8b22e25b3
(w) <wade@tag.bio>
2024-09-03 18:15:39 -0700

    setup-tagbior-notebook-environment.yml: Added. environment.yml, install-package.sh: Updates.

--- install-package.sh
+++ install-package.sh
@@ -2,4 +2,4 @@
 set -ex
 
 echo "Installing tagbio R package itself"
-R CMD INSTALL $TAGBIO_R_UTILS/tagbio_latest.tgz
+mamba run -n $(echo $CONDA_DEFAULT_ENV) R CMD INSTALL $TAGBIO_R_UTILS/tagbio_latest.tgz
