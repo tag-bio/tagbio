300f9c65fed33fda2cb70ca0c2c7ad37bd4b5c62
(w) <wade@tag.bio>
2025-08-13 10:10:57 -0700

    dependencies: allow r-base flexible solve downstream

--- install-package.sh
+++ install-package.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 set -ex
 
-echo "Installing tagbio R package itself"
-mamba run -n $(echo $CONDA_DEFAULT_ENV) R CMD INSTALL $TAGBIO_R_UTILS/tagbio_latest.tgz
+
+eval "$(mamba shell hook --shell bash)"
+
+
+# INSTALL TAGBIOR SDK
+mamba run -n "$(echo $CONDA_DEFAULT_ENV)" R CMD INSTALL "$TAGBIO_R_UTILS"/tagbio_latest.tgz
